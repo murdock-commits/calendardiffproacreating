9ff518fac1d3c3f1a02d320cad41cd22d495de0c
murdock-commits <aayushamoli.123@outlook.com>
2020-02-15 22:09:52 +0530

    Changed a bit of script

--- script.js
+++ script.js
@@ -4,10 +4,11 @@ function runClick(){
             console.log("Click function is working."); 
             var a=new Date(document.getElementById("Date1").value);
             var b=new Date(document.getElementById("Date2").value);
-            console.log(a);
-            console.log(b);
-            var diff=a-b;
+            var c=Date.parse(a);
+            var d=Date.parse(b);
+            var diff=Math.abs(c-d);
             diff=diff/1000;
             console.log(diff);
-            document.getElementById("output").innerHTML=(diff+" seconds");           
-}
+            document.getElementById("output").innerHTML=(diff+" seconds");
+                document.getElementById("output2").innerHTML=(diff*66000*0.2);
+        }
